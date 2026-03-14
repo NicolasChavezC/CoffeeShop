63d635b7ecd29a753b21849cefb1775c46141d7a
NicolasChavezC <iranicazares@gmail.com>
2026-03-13 23:02:28 -0600

    Done

--- controller/controller.py
+++ controller/controller.py
@@ -13,10 +13,36 @@ class LoginController:
         else:
             QtWidgets.QMessageBox.warning(self.window, "Coffee Talk - ERROR","Login keys incorrect")
 
-class Main_To_Menu:
+class Main:
     def __init__(self, window, model):
         self.window = window
         self.model = model
         self.window.btn_cff_Menu_Main.clicked.connect(self.main_to_menu)
+        self.window.btn_cff_Menu_Main_Ad.clicked.connect(self.main_to_menu)
+        self.window.btn_cff_Report_Main.clicked.connect(self.main_to_report)
     def main_to_menu(self):
         self.window.move_main_to_menu.emit()
+    def main_to_report(self):
+        self.window.move_main_to_report.emit()
+
+class Menu:
+    def __init__(self, window, model):
+        self.window = window
+        self.model = model
+        self.window.btn_ct_Main_Menu.clicked.connect(self.menu_to_main)
+        self.window.btn_cff_Report_Menu.clicked.connect(self.menu_to_report)
+    def menu_to_main(self):
+        self.window.move_menu_to_main.emit()
+    def menu_to_report(self):
+        self.window.move_menu_to_report.emit()
+
+class Report:
+    def __init__(self, window, model):
+        self.window = window
+        self.model = model
+        self.window.btn_ct_Main_Report.clicked.connect(self.report_to_main)
+        self.window.btn_cff_Menu_Report.clicked.connect(self.report_to_menu)
+    def report_to_main(self):
+        self.window.move_report_to_main.emit()
+    def report_to_menu(self):
+        self.window.move_report_to_menu.emit()
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from PyQt6 import QtWidgets, uic
 import sys
 from controller.controller import LoginController
-from controller.controller import Main_To_Menu
+from controller.controller import Main
+from controller.controller import Menu
+from controller.controller import Report
 from PyQt6.QtCore import pyqtSignal
 from PyQt6.QtGui import QPalette
 
@@ -14,17 +16,23 @@ class coffee_login(QtWidgets.QMainWindow):
 
 class coffee_main(QtWidgets.QDialog):
     move_main_to_menu = pyqtSignal()
+    move_main_to_report = pyqtSignal()
     def __init__(self):
         super().__init__()
         uic.loadUi("./views/main.ui", self)
-        self.controller = Main_To_Menu(self, self)
+        self.controller = Main(self, self)
 
 class coffee_menu(QtWidgets.QDialog):
+    move_menu_to_main = pyqtSignal()
+    move_menu_to_report = pyqtSignal()
     def __init__(self):
         super().__init__()
         uic.loadUi("./views/menu.ui", self)
+        self.controller = Menu(self, self)
 
 class coffee_report(QtWidgets.QDialog):
+    move_report_to_main = pyqtSignal()
+    move_report_to_menu = pyqtSignal()
     def __init__(self):
         super().__init__()
         uic.loadUi("./views/report.ui", self)
@@ -38,12 +46,26 @@ class AppManager:
         self.coffee_login.show()
         self.coffee_login.login_successful.connect(self.show_main_window)
         self.coffee_main.move_main_to_menu.connect(self.menu_to_main)
+        self.coffee_main.move_main_to_report.connect(self.menu_to_report)
+        self.coffee_menu.move_menu_to_main.connect(self.menu_to_main)
+        self.coffee_menu.move_menu_to_report.connect(self.menu_to_report)
+        self.coffee_report.move_report_to_main.connect(self.report_to_main)
+        self.coffee_report.move_report_to_menu.connect(self.report_to_menu)
     def show_main_window(self):
         self.coffee_main.show()
         self.coffee_login.close()
     def menu_to_main(self):
         self.coffee_menu.show()
         self.coffee_main.close()
+    def menu_to_report(self):
+        self.coffee_report.show()
+        self.coffee_main.close()
+    def report_to_main(self):
+        self.coffee_main.show()
+        self.coffee_report.close()
+    def report_to_menu(self):
+        self.coffee_menu.show()
+        self.coffee_report.close()
 
 app = QtWidgets.QApplication(sys.argv)
 manager = AppManager()
